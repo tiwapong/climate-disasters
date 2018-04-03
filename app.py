8c9b3788378ed6e4de23fb952ccd862f743e43bc
Natalie Tiwapong <ntiwapong@knuedge.com>
2018-04-03 00:28:16 -0700

    user input connectivity to db queries OK

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import datetime
 from pymongo import MongoClient
 
 import json
@@ -11,11 +12,24 @@ db = mongo_client.fema
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
     disaster_types = db.disasters_declarations_data.distinct("incidentType")
-    print("Disaster Types: ", disaster_types)
-    return render_template('index.html', disaster_types=disaster_types)
+    return render_template("index.html", t=disaster_types)
+
+@app.route('/', methods=['POST'])
+def inputs_form_submit():
+    disaster = request.form['disaster']
+    startDate = request.form['startDate']
+    endDate = request.form['endDate']
+    start = datetime.strptime(startDate, "%Y-%m-%d")
+    end = datetime.strptime(endDate, "%Y-%m-%d")
+    print("startDateObj: " , start)
+    print("endDateObj: ", end)
+
+    states = db.disasters_declarations_data.distinct("state", { "incidentBeginDate": { "$gte": start }, "incidentEndDate": { "$lte": end }, "incidentType": disaster})
+    print("states", states)
+    return "OK"
 
 if __name__ == "__main__":
     app.run()
