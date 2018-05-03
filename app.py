39e6a18fe0bc6e524aaf891aa48bd2361c8476f1
Natalie Tiwapong <ntiwapong@knuedge.com>
2018-05-03 11:39:15 -0700

    initial commit

--- app.py
+++ app.py
@@ -122,4 +122,6 @@ def after_request(response):
 	return response
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0')
